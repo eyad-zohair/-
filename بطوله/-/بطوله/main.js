6326aaf3fc4995b4de8368dd6fab38b78860df4e
eyad-zohair <149653034+eyad-zohair@users.noreply.github.com>
2025-04-09 14:11:55 +0300

    Add files via upload

--- بطوله/-/بطوله/main.js
+++ بطوله/-/بطوله/main.js
@@ -32,50 +32,18 @@ function changeColors() {
     document.querySelector("h1").style.color = "black";
     document.querySelector(".head").style.backgroundColor = "white";
     document.querySelector(".side-bar").style.backgroundColor = "white";
-    document.querySelectorAll("ul a")[0].style.color = "black";
-    document.querySelectorAll("ul a")[1].style.color = "black";
-    document.querySelectorAll("ul a")[2].style.color = "black";
-    document.querySelectorAll("ul a")[3].style.color = "black";
-    document
-      .querySelectorAll("ul li")[0]
-      .addEventListener("mouseover", function () {
-        document.querySelectorAll("ul li")[0].style.backgroundColor = "#f6f6f6";
+    for (let i = 0; i < 4; i++) {
+      let a = document.querySelectorAll("ul a");
+      let li = document.querySelectorAll("ul li");
+      a[i].style.color = "black";
+      li[i].addEventListener("mouseover", function () {
+        li[i].style.backgroundColor = "#f6f6f6";
       });
-    document
-      .querySelectorAll("ul li")[0]
-      .addEventListener("mouseout", function () {
-        document.querySelectorAll("ul li")[0].style.backgroundColor = "";
+
+      li[i].addEventListener("mouseout", function () {
+        li[i].style.backgroundColor = "";
       });
-    document
-      .querySelectorAll("ul li")[1]
-      .addEventListener("mouseover", function () {
-        document.querySelectorAll("ul li")[1].style.backgroundColor = "#f6f6f6";
-      });
-    document
-      .querySelectorAll("ul li")[1]
-      .addEventListener("mouseout", function () {
-        document.querySelectorAll("ul li")[1].style.backgroundColor = "";
-      });
-    document
-      .querySelectorAll("ul li")[2]
-      .addEventListener("mouseover", function () {
-        document.querySelectorAll("ul li")[2].style.backgroundColor = "#f6f6f6";
-      });
-    document
-      .querySelectorAll("ul li")[2]
-      .addEventListener("mouseout", function () {
-        document.querySelectorAll("ul li")[2].style.backgroundColor = "";
-      });
-    document
-      .querySelectorAll("ul li")[3]
-      .addEventListener("mouseover", function () {
-        document.querySelectorAll("ul li")[3].style.backgroundColor = "#f6f6f6";
-      });
-    document
-      .querySelectorAll("ul li")[3]
-      .addEventListener("mouseout", function () {
-        document.querySelectorAll("ul li")[3].style.backgroundColor = "";
-      });
+    }
     document.querySelector(".active").style.backgroundColor = "#f6f6f6";
     document.querySelector("h3").style.color = "black";
     document.querySelector(".notification i").style.color = "black";
